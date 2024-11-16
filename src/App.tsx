48faf2b9f2aa87988a6226900e28f9d0952ebc91
NCLAN\hahe <hahe@netcompany.com>
2024-11-16 11:43:54 +0100

    Added bootstrap columns

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import HelloThere from "./components/HelloThere";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
          Hello, welcome to the world of Honung :)
         </p>
+          <HelloThere />
 
    {/*     <a
           className="App-link"
